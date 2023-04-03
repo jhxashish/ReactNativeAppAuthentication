037bd065f1b19403a76c6d67ea2043a2c97f8110
Ashish kumar jha <ashishkumarjha810@gmail.com>
2023-04-03 17:16:03 +0530

    first commit

---
+++ utill/auth.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyBgdlvCFxKdafkJZe6pl-u3U8DwHjR39Tw';
+
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  //console.log(response.data);
+
+  const token = response.data.idToken;
+
+  return token;
+}
+
+export  function createUser(email, password) {
+  return authenticate('signUp', email, password);
+  return token;
+}
+
+
+export  function login(email, password) {
+ return authenticate('signInWithPassword', email, password);
+}
